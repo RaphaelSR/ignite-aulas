a93f2fc3388ae96df9003647a07c3478715870e4
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-06-15 17:06:47 -0300

    Utilizando proporções em medidas

--- reactnative/projeto/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/projeto/gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import {Container, Title} from './styles';
+import React from "react";
+import { Text } from "react-native";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
-export function Dashboard(){
-    return (
-        <Container>
-            <Title> Hello World! </Title>
-        </Container>
-    )
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/12202804?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Raphael</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
 }
-
