0acd97421102e5e0c767d68fd1536d4af8e506dc
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-06-22 16:11:32 -0300

    fast refresh configurated

--- reactjs/01-github-explorer/src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem({repository}){
-    return (
-        <li>
-        <strong>{repository.name ?? 'unform'}</strong>
-        <p>{repository.description ?? 'default'}</p>
-        <a href="">{repository.link ?? 'default'}</a>
-    </li>
-    );
-}
