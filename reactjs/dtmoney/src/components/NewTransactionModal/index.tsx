6d51e879470c05fb72f574856a96132af5b8c40d
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-08-11 11:47:51 -0300

    Global modal styles ok

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import Modal from "react-modal";
 
 interface NewTransactionModalProps {
@@ -11,8 +14,32 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar Transação</h2>
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
+        <button type="button" onClick={onRequestClose} className="react-modal-close">
+            <img src={closeImg} alt="Fechar modal" />
+        </button>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+        <TransactionTypeContainer>
+            <button
+            type="button"
+            >
+                <img src={incomeImg} alt="Entrada" />
+                <span>Entrada</span>
+            </button>
+            <button
+            type="button"
+            >
+                <img src={outcomeImg} alt="Entrada" />
+                <span>Saída</span>
+            </button>
+        </TransactionTypeContainer>
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
