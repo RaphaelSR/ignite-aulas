fe93f24fc9a8a6bf520a2cba7113a38fe1ecd4a9
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-08-11 09:40:59 -0300

    Componentizando modal

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import Modal from 'react-modal';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
@@ -17,17 +18,12 @@ function handleOpenNewTransactionModal() {
 function handleCloseNewTransactionModal() {
   setIsNewTransactionModalOpen(false);
 }
+
   return (
     <>
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
      <Dashboard/>
-
-     <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar Transação</h2>
-        </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
      <GlobalStyle/>
     </>
   );
