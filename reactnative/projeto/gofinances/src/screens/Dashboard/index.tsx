121b2c549958f04f07a32fcf304675a04447a15d
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-08-21 17:45:51 -0300

    Cartões de transação

--- reactnative/projeto/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/projeto/gofinances/src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
 import {
   Container,
@@ -12,6 +13,8 @@ import {
   UserName,
   Icon,
   HighlightCards,
+  Transactions,
+  Title
 } from "./styles";
 
 export function Dashboard() {
@@ -34,10 +37,30 @@ export function Dashboard() {
         </UserWrapper>
       </Header>
       <HighlightCards>
-        <HighlightCard  type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril" />
-        <HighlightCard  type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril" />
-        <HighlightCard  type="total" title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" />
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard></TransactionCard>
+      </Transactions>
     </Container>
   );
 }
