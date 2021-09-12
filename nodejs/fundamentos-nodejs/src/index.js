7c281ccfa1acc91b30e22d24a6e2c669c652794b
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-09-12 17:31:19 -0300

    Validando CPF existente

--- nodejs/fundamentos-nodejs/src/index.js
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -11,16 +11,25 @@ const costumers = [];
 
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
-    const id = uuidv4();
+
+    const costumerAlreadyExistis = costumers.some((costumer) => costumer.cpf === cpf)
+
+    if(costumerAlreadyExistis){
+        return response.status(400).json({error: "Costumer already exists! "});
+    }
 
     costumers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
     return response.status(201).send(costumers);
+})
+
+app.get("/account", (request, response) => {
+    return response.status(200).send(costumers);
 })
 
 app.listen(3333, () => {console.log('API Started on localhost:3333')});
