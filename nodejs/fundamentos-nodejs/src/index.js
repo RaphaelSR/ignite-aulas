4274f1a4a82075c2fd8a1528a6b2d8f0a9041aa9
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-09-12 07:39:16 -0300

    Utilizando os métodos HTTP

--- nodejs/fundamentos-nodejs/src/index.js
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -1,12 +1,27 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express');
 
 const app = express();
 
-// localhost:3333
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World!"});
+app.get("/courses", (request, response) => {
+    return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
+})
+
+app.post("/courses", (request, response) => {
+    return response.json(['Curso 1', 'Curso 2', 'Curso 3' , 'Curso 4']);
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(['Curso 6', 'Curso 2', 'Curso 3' , 'Curso 4']);
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(['Curso 1', 'Curso 2', 'Curso 3' , 'Curso 4']);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(['Curso 1', 'Curso 2', 'Curso 4']);
 })
 
 app.listen(3333, () => {console.log('API Started on localhost:3333')});
