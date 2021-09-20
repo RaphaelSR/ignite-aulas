b473be99b70c8da88e556fded0b69b1ae041b2f4
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-09-20 11:28:33 -0300

    Component button

--- reactnative/projeto/gofinances/src/screens/Register/index.tsx
+++ reactnative/projeto/gofinances/src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Input } from "../../components/Forms/Input";
-import { Container, Header, Title, Form } from "./styles";
+import { Button } from "../../components/Forms/Button";
+import { Container, Header, Title, Form, Fields } from "./styles";
 
 export function Register() {
   return (
@@ -10,9 +11,12 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
+        <Fields>
+          <Input placeholder="Nome" />
 
-        <Input placeholder="Preço" />
+          <Input placeholder="Preço" />
+        </Fields>
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
