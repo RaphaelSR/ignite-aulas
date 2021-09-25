60a63b3684842b25a82230a2becb5ea007d0aa8a
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-09-25 00:25:02 -0300

    Validação de Formulário
    https://react-hook-form.com/get-started
    schema validation with yup

--- reactnative/projeto/gofinances/src/screens/Register/index.tsx
+++ reactnative/projeto/gofinances/src/screens/Register/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Modal } from 'react-native';
+import { Keyboard, Modal, TouchableWithoutFeedback, Alert } from 'react-native';
+import  * as Yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
-
 import { Input } from '../../components/Forms/Input';
 import { InputForm } from '../../components/Forms/InputForm';
 import { Button } from '../../components/Forms/Button';
@@ -24,6 +25,16 @@ interface FormData {
   amount: string;  
 }
 
+const schema = Yup.object().shape({
+  name: Yup
+  .string()
+  .required('Nome é obrigatório'),
+  amount: Yup
+  .number()
+  .typeError('Informe um valor númerico')
+  .positive('O valor não pode ser negativo')
+});
+
 export function Register(){
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
@@ -35,8 +46,11 @@ export function Register(){
 
   const {
     control,
-    handleSubmit
-  } = useForm();
+    handleSubmit,
+    formState: {errors}
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
 
   function handleTransactionsTypeSelect(type: 'up' | 'down'){
     setTransactionType(type);
@@ -51,6 +65,12 @@ export function Register(){
   }
 
   function handleRegister(form: FormData){
+    if(!transactionType)
+      return Alert.alert('Selecione o tipo da transação');
+    
+    if(category.key === 'category')
+      return Alert.alert('Selecione a categoria');
+
     const data = {
       name: form.name,
       amount: form.amount,
@@ -62,7 +82,8 @@ export function Register(){
   }
 
   return (
-    <Container>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <Container>      
       <Header>
         <Title>Cadastro</Title>
       </Header>
@@ -73,12 +94,17 @@ export function Register(){
             name="name"
             control={control}
             placeholder="Nome"
+            autoCapitalize="sentences"
+            autoCorrect={false}
+            error={errors.name && errors.name.message}
           />
 
           <InputForm
             name="amount"
             control={control}
             placeholder="Preço"
+            keyboardType="numeric"
+            error={errors.amount && errors.amount.message}
           />
 
           <TransactionTypes>
@@ -116,5 +142,6 @@ export function Register(){
         />
       </Modal>
     </Container>
+    </TouchableWithoutFeedback>
   );
 }
