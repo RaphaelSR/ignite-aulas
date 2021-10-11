e2e5f922feab48394bcb9dc3a507789b81d0f455
RaphaelSR2 <globalsys.raphael@wine.com.br>
2021-10-11 16:33:39 -0300

    Estilização do filtro por mês

--- reactnative/projeto/gofinances/src/screens/Resume/index.tsx
+++ reactnative/projeto/gofinances/src/screens/Resume/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { VictoryPie } from "victory-native";
-
+import { RFValue } from "react-native-responsive-fontsize";
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { HistoryCard } from "../../components/HistoryCard";
 import { useTheme } from "styled-components";
 
-import { Container, Header, Title, Content, ChartContainer } from "./styles";
+import { Container, Header, Title, Content, ChartContainer, MonthSelect, MonthSelectButton, MonthSelectIcon, Month } from "./styles";
 
 import { categories } from "../../utils/categories";
-import { RFValue } from "react-native-responsive-fontsize";
+
 
 interface TransactionData {
   type: "positive" | "negative";
@@ -43,9 +44,12 @@ export function Resume() {
       (expensive: TransactionData) => expensive.type === "negative"
     );
 
-    const expensivesTotal = expensives.reduce((acumulator: number, expensive: TransactionData) =>{
-      return acumulator + Number(expensive.amount);
-    }, 0);
+    const expensivesTotal = expensives.reduce(
+      (acumulator: number, expensive: TransactionData) => {
+        return acumulator + Number(expensive.amount);
+      },
+      0
+    );
 
     const totalByCategory: CategoryData[] = [];
 
@@ -64,7 +68,9 @@ export function Resume() {
           currency: "BRL",
         });
 
-        const percent = `${(categorySum / expensivesTotal * 100).toFixed(0)}%`;
+        const percent = `${((categorySum / expensivesTotal) * 100).toFixed(
+          0
+        )}%`;
 
         totalByCategory.push({
           key: category.key,
@@ -90,20 +96,41 @@ export function Resume() {
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <Content>
+      <Content
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: useBottomTabBarHeight(),
+        }}
+      >
+
+        <MonthSelect>
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-left"/>
+          </MonthSelectButton>
+
+          <Month>Maio</Month>
+
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-right"/>
+          </MonthSelectButton>
+        </MonthSelect>
+
         <ChartContainer>
-          <VictoryPie 
-          data={totalByCategories} 
-          colorScale={totalByCategories.map(category => category.color)} 
-          style={{
-            labels: {
-              fontSize: RFValue(18),
-              fontWeight: 'bold',
-              fill: theme.colors.shape
-            } 
-          }}
-          labelRadius={50}
-          x="percent" y="total" />
+          <VictoryPie
+            data={totalByCategories}
+            colorScale={totalByCategories.map((category) => category.color)}
+            style={{
+              labels: {
+                fontSize: RFValue(18),
+                fontWeight: "bold",
+                fill: theme.colors.shape,
+              },
+            }}
+            labelRadius={50}
+            x="percent"
+            y="total"
+          />
         </ChartContainer>
 
         {totalByCategories.map((item) => (
