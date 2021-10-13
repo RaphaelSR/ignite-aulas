98f6a315faa51fd931d06556430a10d42506ab52
RaphaelSR2 <globalsys.raphael@wine.com.br>
2021-10-13 18:41:53 -0300

    Criando contexto

--- reactnative/projeto/gofinances/App.tsx
+++ reactnative/projeto/gofinances/App.tsx
@@ -1,44 +1,46 @@
-import 'react-native-gesture-handler';
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
+import "react-native-gesture-handler";
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
 
-import React from 'react';
-import { StatusBar } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';
-
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+import { AuthContext } from "./src/AuthContext";
+import { NavigationContainer } from "@react-navigation/native";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins';
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme';
+import theme from "./src/global/styles/theme";
 
-import { AppRoutes } from './src/routes/app.routes';
+import { AppRoutes } from "./src/routes/app.routes";
 
-import {Signin} from './src/screens/Signin';
+import { Signin } from "./src/screens/Signin";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
 
-  if(!fontsLoaded){
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <StatusBar barStyle='light-content'/>
-        <Signin />
+        <StatusBar barStyle="light-content" />
+        <AuthContext.Provider value={[]}>
+          <Signin />
+        </AuthContext.Provider>
       </NavigationContainer>
     </ThemeProvider>
-  )
+  );
 }
--- reactnative/projeto/gofinances/src/screens/Signin/index.tsx
+++ reactnative/projeto/gofinances/src/screens/Signin/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
 import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
 import LogoSvg from '../../assets/logo.svg';
+
+import { AuthContext } from '../../AuthContext';
+
 import { SignInSocialButton } from '../../components/SignInSocialButton';
 import { Container, Header, TitleWrapper, Title, SignInTitle, Footer, FooterWrapper } from './styles';
 
 export function Signin(){
+    const data = useContext(AuthContext);
+    console.log(data);
     return(
         <Container>
             <Header>
