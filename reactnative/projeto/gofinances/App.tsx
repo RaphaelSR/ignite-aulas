8104c34908c351ffef1b915931724f4185083d9b
RaphaelSR2 <globalsys.raphael@wine.com.br>
2021-11-15 20:20:37 -0300

    Definindo as Rotas Privadas e Públicas

--- reactnative/projeto/gofinances/App.tsx
+++ reactnative/projeto/gofinances/App.tsx
@@ -7,7 +7,7 @@ import { StatusBar } from "react-native";
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components";
 import { AuthProvider } from "./src/hooks/auth";
-import { NavigationContainer } from "@react-navigation/native";
+import  { Routes } from "./src/routes";
 
 import {
   useFonts,
@@ -35,12 +35,10 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
         <StatusBar barStyle="light-content" />
         <AuthProvider>
-          <Signin />
+          <Routes />
         </AuthProvider>
-      </NavigationContainer>
     </ThemeProvider>
   );
 }
